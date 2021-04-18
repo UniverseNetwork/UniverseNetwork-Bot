7c7ecdc1744e96993aa8da692f8a5d838e68ebaf
ARVIN3108 <31arvin08@gmail.com>
2021-04-18 07:15:30 +0700

    Optimized Ping Commands

--- commands/ping.js
+++ commands/ping.js
@@ -2,21 +2,16 @@ module.exports = {
     name: 'ping',
     description: 'To Calculate Bot & API ping',
     execute(message, client, MessageEmbed, Prefix, Icon) {
-        message.delete()
+        message.delete();
         message.channel.send(':hourglass: **Menghitung Ping Dalam 3 Detik...**').then(resultMessage => {
-            const ping = resultMessage.createdTimestamp - message.createdTimestamp
-
-            setTimeout(function () {
-                const embed = new MessageEmbed()
-                    .setAuthor('UniversNetwork', client.user.displayAvatarURL({ dynamic: true }), 'https://minecraft-mp.com/server-s272254')
-                    .setColor('#15FF02')
-                    .setTitle('**Prefix:** `' + Prefix + '`')
-                    .setDescription(':clipboard: **Diminta Oleh** <@' + message.author + '>\n\u200B\n\u200B')
-                    .addField('🤖 Bot Latency', ping, true)
-                    .addField('🌐 WebSocket Latency', client.ws.ping, true)
-                    .setFooter('Made By ARVIN3108 ID', Icon)
-                resultMessage.edit('', embed)
-            }, 3000)
-        })
+            setTimeout(() => resultMessage.edit('', new MessageEmbed()
+                .setAuthor('UniversNetwork', client.user.displayAvatarURL({ dynamic: true }), 'https://minecraft-mp.com/server-s272254')
+                .setColor('#15FF02')
+                .setTitle('**Prefix:** `' + Prefix + '`')
+                .setDescription(':clipboard: **Diminta Oleh** <@' + message.author + '>\n\u200B\n\u200B')
+                .addField('🤖 Bot Latency', resultMessage.createdTimestamp - message.createdTimestamp, true)
+                .addField('🌐 WebSocket Latency', client.ws.ping, true)
+                .setFooter('Made By ARVIN3108 ID', Icon)), 3000);
+        });
     }
-}
+};
