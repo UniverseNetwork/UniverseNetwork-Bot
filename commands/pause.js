2258a9ac80d8bd054a53da0ebe0e4cdbaee76e23
ARVIN3108 <31arvin08@gmail.com>
2021-05-31 20:40:13 +0700

    Pause & Resume Mechanis Changed

--- commands/pause.js
+++ commands/pause.js
@@ -1,14 +1,25 @@
+let { Message } = require('discord.js');
 module.exports = {
     name: 'pause',
-    description: 'To Pause a song',
-    async execute(message, args, distube) {
-        let queue = await distube.getQueue(message)
-
-        if (queue) {
-            distube.pause(message)
-            message.delete().then(message.channel.send(':pause_button: **Menjeda Lagu!**'))
-        } else if (!queue) {
-            message.delete().then(message.channel.send(':x: **Kamu tidak sedang memutar lagu!**'))
+    async run(m) {
+        m.delete(); let d = require('../Modules/DisTube'), g = require('../Modules/Client').guilds.cache.get('761872006513033238'),
+            o = { username: `${g.name} Song Player`, avatarURL: 'https://i.imgur.com/pBmA5S6.png' }, q = await d.getQueue(m),
+            wh = await m.channel.fetchWebhooks(), w = wh.first(), { MessageEmbed } = require('discord.js');
+        if (q) {
+            d.pause(m); w.send({
+                username: o.username,
+                avatarURL: o.avatarURL,
+                embeds: [new MessageEmbed()
+                    .setTitle('**Prefix:** `' + await require('../Functions/Handlers').prefix(m) + '`')
+                    .setColor('#02C2FF')
+                    .setAuthor(g.name, g.iconURL({ dynamic: true }), 'https://minecraft-mp.com/server-s272254')
+                    .setDescription(':clipboard: **Diminta Oleh** <@' + m.author + '>\n\u200B')
+                    .addField('\u200B', '\u200B', true).addField('**Song Player**', '\u200B', true)
+                    .addField('\u200B', '\u200B', true).addField(':pause_button: **Menjeda Lagu!**', '\u200B')
+                    .setFooter('Made By ARVIN3108 ID', process.env.Icon || require('../config.json').Icon)]
+            })
+        } else if (!q) {
+            w.send(':x: **Bot tidak sedang memutar lagu!**', o)
         }
     }
 }
--- commands/resume.js
+++ commands/resume.js
@@ -1,14 +1,23 @@
 module.exports = {
     name: 'resume',
-    description: 'To resume a song',
-    async execute(message, args, distube) {
-        let queue = await distube.getQueue(message)
-
-        if (queue) {
-            distube.resume(message)
-            message.delete().then(message.channel.send(':arrow_forward: **Melanjutkan Lagu!**'))
-        } else if (!queue) {
-            message.delete().then(message.channel.send(':x: **Kamu tidak sedang memutar lagu!**'))
-        }
+    async run(m) {
+        m.delete
+        let d = require('../Modules/DisTube'), g = require('../Modules/Client').guilds.cache.get('761872006513033238'),
+            { MessageEmbed } = require('discord.js'), wh = await m.channel.fetchWebhooks(), w = wh.first(), q = await d.getQueue(message),
+            o = { username: `${g.name} Song Player`, avatarURL: 'https://i.imgur.com/pBmA5S6.png' };
+        if (q) {
+            d.resume(m); w.send({
+                username: o.username,
+                avatarURL: o.avatarURL,
+                embeds: [new MessageEmbed()
+                    .setTitle('**Prefix:** `' + await require('../Functions/Handlers').prefix(m) + '`')
+                    .setColor('#02C2FF')
+                    .setAuthor(g.name, g.iconURL({ dynamic: true }), 'https://minecraft-mp.com/server-s272254')
+                    .setDescription(':clipboard: **Diminta Oleh** <@' + m.author + '>\n\u200B')
+                    .addField('\u200B', '\u200B', true).addField('**Song Player**', '\u200B', true)
+                    .addField('\u200B', '\u200B', true).addField(':arrow_forward: **Melanjutkan Lagu!**', '\u200B')
+                    .setFooter('Made By ARVIN3108 ID', process.env.Icon || require('../config.json').Icon)]
+            });
+        } else if (!q) w.send(':x: **Bot tidak sedang memutar lagu!**', o);
     }
 }
