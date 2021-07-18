ca8988e10688c734379054f796ef001883eea62b
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2021-07-18 18:40:09 +0700

    Updated to djs 13 format

--- commands/ip.js
+++ commands/ip.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ip',
     description: 'This is a ip command',
     run(m) {
-        m.delete().then(m.channel.send('**IP: play.universnetwork.xyz**'))
+        m.delete().then(m.channel.send({content: '**IP: play.universenetwork.id**'}))
     }
 }
