12d9205ba9723bd33b018ea45baaffe389ec9cd9
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2021-07-29 10:01:01 +0700

    Update discord.js and mongoose

--- commands/whatsapp.js
+++ commands/whatsapp.js
@@ -1,5 +1,10 @@
 module.exports = {
-    name: 'whatsapp', alias: 'wa', description: 'To send a whatsapp link', run(m) {
-        m.delete().then(m.channel.send({ content: '**WhatsApp:** https://chat.whatsapp.com/DNG8OO7XFu8Kb5IyMxyQ7N' }))
+    name: 'whatsapp', alias: 'wa', description: 'To send a whatsapp link', async run(m) {
+        let w = await m.channel.fetchWebhooks();
+        m.delete().then(w.first().send({
+            content: '**WhatsApp:** https://chat.whatsapp.com/DNG8OO7XFu8Kb5IyMxyQ7N',
+            username: `${m.client.guilds.cache.get('761872006513033238').name} WhatsApp Inviters`,
+            avatarURL: 'https://i.imgur.com/hP6VsJP.jpg'
+        }))
     }
 }
